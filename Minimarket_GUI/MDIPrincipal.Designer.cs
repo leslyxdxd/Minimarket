beefc06cc24458d71e79644b06a9bea9871a01a6
JordanVelito <YUNZU2255@DESKTOP-1RLBNU3>
2024-04-21 17:45:44 -0500

    cambio

---
+++ Minimarket_GUI/MDIPrincipal.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace ProyVentas_GUI
+{
+    partial class MDIPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            mantenimientosToolStripMenuItem = new ToolStripMenuItem();
+            proveedoresToolStripMenuItem = new ToolStripMenuItem();
+            vendedoresToolStripMenuItem = new ToolStripMenuItem();
+            consultasToolStripMenuItem = new ToolStripMenuItem();
+            facturasClienteToolStripMenuItem = new ToolStripMenuItem();
+            facturasVendedorToolStripMenuItem = new ToolStripMenuItem();
+            listadosToolStripMenuItem = new ToolStripMenuItem();
+            listadosXMLToolStripMenuItem = new ToolStripMenuItem();
+            listadosExcelToolStripMenuItem = new ToolStripMenuItem();
+            salirDelSistemasToolStripMenuItem = new ToolStripMenuItem();
+            salirToolStripMenuItem = new ToolStripMenuItem();
+            statusStrip1 = new StatusStrip();
+            lbl1 = new ToolStripStatusLabel();
+            lblUsuario = new ToolStripStatusLabel();
+            productoToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            statusStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { mantenimientosToolStripMenuItem, consultasToolStripMenuItem, listadosToolStripMenuItem, salirDelSistemasToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Padding = new Padding(7, 2, 0, 2);
+            menuStrip1.Size = new Size(1059, 24);
+            menuStrip1.TabIndex = 1;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // mantenimientosToolStripMenuItem
+            // 
+            mantenimientosToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { proveedoresToolStripMenuItem, vendedoresToolStripMenuItem, productoToolStripMenuItem });
+            mantenimientosToolStripMenuItem.Name = "mantenimientosToolStripMenuItem";
+            mantenimientosToolStripMenuItem.Size = new Size(106, 20);
+            mantenimientosToolStripMenuItem.Text = "Mantenimientos";
+            // 
+            // proveedoresToolStripMenuItem
+            // 
+            proveedoresToolStripMenuItem.Name = "proveedoresToolStripMenuItem";
+            proveedoresToolStripMenuItem.Size = new Size(180, 22);
+            proveedoresToolStripMenuItem.Text = "Proveedores";
+            proveedoresToolStripMenuItem.Click += proveedoresToolStripMenuItem_Click;
+            // 
+            // vendedoresToolStripMenuItem
+            // 
+            vendedoresToolStripMenuItem.Name = "vendedoresToolStripMenuItem";
+            vendedoresToolStripMenuItem.Size = new Size(180, 22);
+            vendedoresToolStripMenuItem.Text = "Vendedores";
+            vendedoresToolStripMenuItem.Click += vendedoresToolStripMenuItem_Click;
+            // 
+            // consultasToolStripMenuItem
+            // 
+            consultasToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { facturasClienteToolStripMenuItem, facturasVendedorToolStripMenuItem });
+            consultasToolStripMenuItem.Name = "consultasToolStripMenuItem";
+            consultasToolStripMenuItem.Size = new Size(71, 20);
+            consultasToolStripMenuItem.Text = "Consultas";
+            // 
+            // facturasClienteToolStripMenuItem
+            // 
+            facturasClienteToolStripMenuItem.Name = "facturasClienteToolStripMenuItem";
+            facturasClienteToolStripMenuItem.Size = new Size(171, 22);
+            facturasClienteToolStripMenuItem.Text = "Facturas Cliente";
+            facturasClienteToolStripMenuItem.Click += facturasClienteToolStripMenuItem_Click;
+            // 
+            // facturasVendedorToolStripMenuItem
+            // 
+            facturasVendedorToolStripMenuItem.Name = "facturasVendedorToolStripMenuItem";
+            facturasVendedorToolStripMenuItem.Size = new Size(171, 22);
+            facturasVendedorToolStripMenuItem.Text = "Facturas Vendedor";
+            // 
+            // listadosToolStripMenuItem
+            // 
+            listadosToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { listadosXMLToolStripMenuItem, listadosExcelToolStripMenuItem });
+            listadosToolStripMenuItem.Name = "listadosToolStripMenuItem";
+            listadosToolStripMenuItem.Size = new Size(62, 20);
+            listadosToolStripMenuItem.Text = "Listados";
+            // 
+            // listadosXMLToolStripMenuItem
+            // 
+            listadosXMLToolStripMenuItem.Name = "listadosXMLToolStripMenuItem";
+            listadosXMLToolStripMenuItem.Size = new Size(147, 22);
+            listadosXMLToolStripMenuItem.Text = "Listados XML";
+            listadosXMLToolStripMenuItem.Click += listadosXMLToolStripMenuItem_Click;
+            // 
+            // listadosExcelToolStripMenuItem
+            // 
+            listadosExcelToolStripMenuItem.Name = "listadosExcelToolStripMenuItem";
+            listadosExcelToolStripMenuItem.Size = new Size(147, 22);
+            listadosExcelToolStripMenuItem.Text = "Listados Excel";
+            listadosExcelToolStripMenuItem.Click += listadosExcelToolStripMenuItem_Click;
+            // 
+            // salirDelSistemasToolStripMenuItem
+            // 
+            salirDelSistemasToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { salirToolStripMenuItem });
+            salirDelSistemasToolStripMenuItem.Name = "salirDelSistemasToolStripMenuItem";
+            salirDelSistemasToolStripMenuItem.Size = new Size(104, 20);
+            salirDelSistemasToolStripMenuItem.Text = "Salir del Sistema";
+            // 
+            // salirToolStripMenuItem
+            // 
+            salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            salirToolStripMenuItem.Size = new Size(96, 22);
+            salirToolStripMenuItem.Text = "Salir";
+            // 
+            // statusStrip1
+            // 
+            statusStrip1.Items.AddRange(new ToolStripItem[] { lbl1, lblUsuario });
+            statusStrip1.Location = new Point(0, 497);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Padding = new Padding(1, 0, 16, 0);
+            statusStrip1.Size = new Size(1059, 22);
+            statusStrip1.TabIndex = 3;
+            statusStrip1.Text = "statusStrip1";
+            // 
+            // lbl1
+            // 
+            lbl1.BackColor = SystemColors.ActiveCaptionText;
+            lbl1.ForeColor = SystemColors.ButtonHighlight;
+            lbl1.Name = "lbl1";
+            lbl1.Size = new Size(50, 17);
+            lbl1.Text = "Usuario:";
+            // 
+            // lblUsuario
+            // 
+            lblUsuario.BackColor = SystemColors.ActiveCaption;
+            lblUsuario.ForeColor = SystemColors.ButtonHighlight;
+            lblUsuario.Name = "lblUsuario";
+            lblUsuario.Size = new Size(19, 17);
+            lblUsuario.Text = "....";
+            // 
+            // productoToolStripMenuItem
+            // 
+            productoToolStripMenuItem.Name = "productoToolStripMenuItem";
+            productoToolStripMenuItem.Size = new Size(180, 22);
+            productoToolStripMenuItem.Text = "Producto";
+            productoToolStripMenuItem.Click += productoToolStripMenuItem_Click;
+            // 
+            // MDIPrincipal
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1059, 519);
+            Controls.Add(statusStrip1);
+            Controls.Add(menuStrip1);
+            IsMdiContainer = true;
+            MainMenuStrip = menuStrip1;
+            Margin = new Padding(4, 3, 4, 3);
+            Name = "MDIPrincipal";
+            Text = "Menu Principal - Modulo de Ventas";
+            WindowState = FormWindowState.Maximized;
+            FormClosing += MDIPrincipal_FormClosing;
+            FormClosed += MDIPrincipal_FormClosed;
+            Load += MDIPrincipal_Load;
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            statusStrip1.ResumeLayout(false);
+            statusStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem mantenimientosToolStripMenuItem;
+        private ToolStripMenuItem proveedoresToolStripMenuItem;
+        private ToolStripMenuItem vendedoresToolStripMenuItem;
+        private ToolStripMenuItem consultasToolStripMenuItem;
+        private ToolStripMenuItem facturasClienteToolStripMenuItem;
+        private ToolStripMenuItem facturasVendedorToolStripMenuItem;
+        private ToolStripMenuItem listadosToolStripMenuItem;
+        private ToolStripMenuItem listadosXMLToolStripMenuItem;
+        private ToolStripMenuItem listadosExcelToolStripMenuItem;
+        private StatusStrip statusStrip1;
+        private ToolStripStatusLabel lbl1;
+        private ToolStripStatusLabel lblUsuario;
+        private ToolStripMenuItem salirDelSistemasToolStripMenuItem;
+        private ToolStripMenuItem salirToolStripMenuItem;
+        private ToolStripMenuItem productoToolStripMenuItem;
+    }
+}
---
+++ Minimarket_GUI/MDIPrincipal.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyVentas_GUI
+{
+    public partial class MDIPrincipal : Form
+    {
+        public MDIPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void proveedoresToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //ProveedorMan01 prov01 = new ProveedorMan01();
+            //prov01.MdiParent = this;
+            //prov01.Show();
+        }
+
+        private void vendedoresToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //VendedorMan01 vend01 = new VendedorMan01();
+            //vend01.MdiParent = this;
+            //vend01.Show();
+        }
+
+        private void facturasClienteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //frmConsFacturasCliente frmc1 = new frmConsFacturasCliente();
+            //frmc1.MdiParent = this;
+            //frmc1.Show();
+
+        }
+
+        private void listadosXMLToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //frmXMLDemo_01 frmxml01 = new frmXMLDemo_01();
+            //frmxml01.MdiParent = this;
+            //frmxml01.Show();
+        }
+
+        private void listadosExcelToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //frmListadosExcel frmExcel = new frmListadosExcel();
+            //frmExcel.MdiParent = this;
+            //frmExcel.Show();
+        }
+
+        private void MDIPrincipal_Load(object sender, EventArgs e)
+        {
+           /* this.lblUsuario.Text = clsCredenciales.Usuario;
+            //Definimos la seguridad por roles..
+            if (clsCredenciales.Nivel == 1)
+            {
+                mantenimientosToolStripMenuItem.Visible = true;
+                consultasToolStripMenuItem.Visible = true;
+                listadosToolStripMenuItem.Visible = true;
+                salirDelSistemasToolStripMenuItem.Visible = true;
+
+            }
+            else if (clsCredenciales.Nivel == 2)
+            {
+
+                mantenimientosToolStripMenuItem.Visible = true;
+                consultasToolStripMenuItem.Visible = true;
+                listadosToolStripMenuItem.Visible = false;
+                salirDelSistemasToolStripMenuItem.Visible = true;
+
+            }*/
+
+
+        }
+
+        private void MDIPrincipal_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult vrpta;
+            vrpta = MessageBox.Show("Seguro de salir del aplicativo?", "Mensaje", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (vrpta == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+
+        }
+
+        private void MDIPrincipal_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void productoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            /*ProductoMan01 prod01 = new ProductoMan01();
+            prod01.MdiParent = this;
+            prod01.Show();*/
+        }
+    }
+}
