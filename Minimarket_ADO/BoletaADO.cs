f173d870b1a05bcbf2b366c07b8ea4360a83c5a0
leslyxdxd <leslyfiorella1617@outlook.com>
2024-06-10 22:53:15 -0500

    ult mod Lesly

--- Minimarket_ADO/BoletaADO.cs
+++ Minimarket_ADO/BoletaADO.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Minimarket_BE;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,11 @@ namespace Minimarket_ADO
 {
     public class BoletaADO
     {
+        public bool RegistrarVenta(BoletaBE objVenta, DataTable DetalleVenta, out String Mensaje) 
+        {
+        
+        
+        
+        }
     }
 }
